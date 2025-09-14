d4d7bbf0ae4b45f02e304bfd1f7242257d333b81
Илья <osennijila@gmail.com>
2025-09-14 17:21:11 +0300

    Update task_2.cpp

--- task_2.cpp
+++ task_2.cpp
@@ -4,3 +4,18 @@ using namespace std;
 int main() {
 	list<int> numbers{ 1, 2, 3, 4, 5, 6 };
 }
+
+
+#include <vector>
+#include <iostream>
+
+class Stack {
+private:
+    std::vector<int> items;
+public:
+    void push(int item) { items.push_back(item); }
+    int pop() { int top = items.back(); items.pop_back(); return top; }
+    int peek() const { return items.back(); }
+    size_t size() const { return items.size(); }
+};
+
